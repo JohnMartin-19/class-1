3fb43f54aeae1996e11a118cdb33717b1041ba3f
John <johnnkonge2020@gmail.com>
2023-11-30 07:49:38 +0300

    done

--- oop.py
+++ oop.py
@@ -10,8 +10,22 @@
 class Engineers:
     def __init__(self,names):
         print(f'{names} is an Engineer')
-        #self.names = names
+        self.names = names
 engineer1 = Engineers('Zac')
 engineer2 = Engineers('Amina')
 engineer3 = Engineers('Neemat')
 engineer4 = Engineers('John')
+
+
+#optional __init() args
+
+class Animals:
+    def __init__(self, name, age = 35):
+        self.name = name
+        self.age = age
+
+duck = Animals('Bird')
+cat = Animals('Kitty', 7)
+print(cat.age)
+print(cat.name)
+print(duck.age)
